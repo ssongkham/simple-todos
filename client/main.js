a734f9dfbda2c0c5e40b37fb1ba538ce55d1765b
ssongkham <33530065+ssongkham@users.noreply.github.com>
2017-11-13 23:21:45 -0600

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -1,2 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { GoogleMaps } from 'meteor/dburles:google-maps';
 import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
+
+Meteor.startup(function () {
+  	GoogleMaps.load({ key: '',libraries: 'places', });
+	});
+
